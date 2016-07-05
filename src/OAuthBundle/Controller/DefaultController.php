a8f4656f9bc2e1a0dcb6ca5d1d506ddc7780707c
WermooZ <p.weremczuk@gmail.com>
2016-07-05 23:18:57 +0200

    up

--- src/OAuthBundle/Controller/DefaultController.php
+++ src/OAuthBundle/Controller/DefaultController.php
@@ -14,19 +14,15 @@ class DefaultController extends Controller
         $clientManager = $this->get('fos_oauth_server.client_manager.default');
 
         $client = $clientManager->createClient();
-
         $client->setRedirectUris(array('127.0.0.1'));
         $client->setAllowedGrantTypes(array('token', 'authorization_code', 'password', 'client_credentials'));
         $clientManager->updateClient($client);
 
-        $output = sprintf("Added client with id: %s secret: %s",$client->getPublicId(),$client->getSecret());
+        $this->addFlash(
+            'notice',
+            'Added client with ID: '.$client->getPublicId(). ' SECRET: '.$client->getSecret()
+        );
 
-        return new Response($output);
-
-//        return $this->redirect($this->generateUrl('fos_oauth_server_authorize', array(
-//            'client_id'     => $client->getPublicId(),
-//            'redirect_uri'  => 'http://www.example.com',
-//            'response_type' => 'code'
-//        )));
+        return $this->redirectToRoute('homepage');
     }
 }
--- src/UserBundle/Controller/UserController.php
+++ src/UserBundle/Controller/UserController.php
@@ -3,16 +3,11 @@
 namespace UserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-
-use UserBundle\Entity\User;
 
 class UserController extends Controller
 {
     public function indexAction()
     {
-     
-        
         return $this->render('UserBundle:User:index.html.twig');
     }
 }
